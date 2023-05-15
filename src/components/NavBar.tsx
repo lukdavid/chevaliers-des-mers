c5b392085909a0fdde829536aca5976dc20eb54d
Lucas David <lucasdavid@hotmail.fr>
2023-05-15 13:37:27 +0200

    feat: logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,8 +5,8 @@ const NavBar = () => (
     <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-8 py-2 md:py-4">
       <a href="#" className="flex items-center">
         <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 md:h-8"
+          src="/logo_casque.svg"
+          className="mr-3 h-6 invert md:h-12"
           alt="Logo"
         />
         <span className="self-center whitespace-nowrap text-lg font-semibold text-white md:text-2xl">

640e06b635cd869271ce876766f5a63e197b8cf0
Lucas David <lucasdavid@hotmail.fr>
2023-06-02 11:11:21 +0200

    fix typo in gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -7,7 +7,7 @@ const Gallery = () => (
   <div className="relative h-screen px-4 py-48" id={ANCHORS.gallery}>
     <div className="m-auto  max-w-4xl">
       <h1 className=" text-2xl  font-extrabold leading-none tracking-tight text-blue-200 md:text-4xl lg:text-5xl">
-        Gallerie
+        Galerie
       </h1>
       <p className="mb-4 text-blue-300">Quelques photos de nos plongées…</p>
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -55,7 +55,7 @@ const NavBar = () => (
               href={`#${ANCHORS.gallery}`}
               className="block rounded py-2 pl-3 pr-4 text-gray-100 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
             >
-              Gallerie
+              Galerie
             </a>
           </li>
           <li>
